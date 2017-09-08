8779b5d2c16c80b6d85d2b2f434969e109e64791
Derek Weitzel <djw8605@gmail.com>
2017-09-08 16:04:13 -0500

    Convert bytes to str

--- src/scitokens/scitokens.py
+++ src/scitokens/scitokens.py
@@ -29,7 +29,7 @@ def decode_base64(data):
     """
     missing_padding = len(data) % 4
     if missing_padding != 0:
-        data += (b'='* (4 - missing_padding)).encode("ascii")
+        data += str(b'='* (4 - missing_padding))
     return base64.urlsafe_b64decode(data.encode("ascii"))
 
 class MissingKeyException(Exception):
