ca9edbb6d78dc974271ea212626a2ff437052506
Derek Weitzel <djw8605@gmail.com>
2017-09-22 13:50:14 -0500

    Addressing comments in pull request #31

--- src/scitokens/utils/__init__.py
+++ src/scitokens/utils/__init__.py
@@ -25,3 +25,16 @@ def decode_base64(data):
         data += b'='* (4 - missing_padding)
 
     return base64.urlsafe_b64decode(data)
+
+def string_from_long(data):
+    """
+    Create a base64 encoded string for an integer
+    """
+    return base64.urlsafe_b64encode(cryptography.utils.int_to_bytes(data)).decode('ascii')
+
+
+def bytes_from_long(data):
+    """
+    Create a base64 encoded bytes for an integer
+    """
+    return base64.urlsafe_b64encode(cryptography.utils.int_to_bytes(data))
