d30cb10ef9cdaf3d09cdd90db29a6d446f01918d
Brian Bockelman <bbockelm@cse.unl.edu>
2017-09-25 21:16:26 -0500

    Remove unused code around parent web tokens.

--- src/scitokens/scitokens.py
+++ src/scitokens/scitokens.py
@@ -219,62 +219,6 @@ class SciToken(object):
         to_return._verified_claims = claims
         to_return._serialized_token = serialized_token
         return to_return
-        
-        
-        # Clean up all of the below
-
-        key_decoded = base64.urlsafe_b64decode(key)
-        jwk_dict = json.loads(key_decoded)
-        # TODO: Full range of keytypes and curves from JWK RFC.
-        if (jwk_dict['kty'] != 'EC') or (jwt_dict['crv'] != 'P-256'):
-            raise UnsupportedKeyException("SciToken signed with an unsupported key type")
-        elif 'd' not in jwk_dict:
-            raise UnsupportedKeyException("SciToken key does not contain private number.")
-
-        if 'pwt' in unverified_headers:
-            pwt = unverified_headers['pwt']
-            st = SciToken.clone()
-            st.deserialize(pwt, require_key=False)
-            headers = pwt.headers()
-            if 'cwk' not in headers:
-                raise InvalidParentToken("Parent token MUST specify a child JWK.")
-            # Validate the key type / curve matches.  TODO: what other headers to check?
-            if (jwk_dict['kty'] != headers['kty']) or (jwk_dict['crv'] != headers['crv']):
-                if 'x' not in jwk_dict:
-                    if 'x' in headers:
-                        jwk_dict['x'] = headers['x']
-                    else:
-                        MissingPublicKeyException("JWK public key is missing 'x'")
-                elif jwk_dict['x'] != headers['x']:
-                    raise UnsupportedKeyException("Parent SciToken specifies an incompatible child JWK")
-                if 'y' not in jwk_dict:
-                    if 'y' in headers:
-                        jwk_dict['y'] = headers['y']
-                    else:
-                        MissingPublicKeyException("JWK public key is missing 'y'")
-                elif jwk_dict['y'] != headers['y']:
-                    raise UnsupportedKeyException("Parent SciToken specifies an incompatible child JWK")
-        # TODO: Handle non-chained case.
-        elif 'x5u' in unverified_headers:
-            raise NotImplementedError("Non-chained verification is not implemented.")
-        else:
-            raise UnableToValidate("No token validation method available.")
-
-        public_key_numbers = ec.EllipticCurvePublicNumbers(
-               long_from_bytes(jwk_dict['x']),
-               long_from_bytes(jwk_dict['y']),
-               ec.SECP256R1
-           )
-        private_key_numbers = ec.EllipticCurvePrivateNumbers(
-           long_from_bytes(jwk_dict['d']),
-           public_key_numbers
-        )
-        private_key = private_key_numbers.private_key(backends.default_backend())
-        public_key  = public_key_numbers.public_key(backends.default_backend())
-
-        # TODO: check that public and private key match?
-
-        claims = jwt.decode(serialized_token, public_key, algorithm="EC256")
 
 
 class ValidationFailure(Exception):
