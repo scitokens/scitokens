70c9c85c74ba0251a1d54b64d133bc9ee266e854
Derek Weitzel <djw8605@gmail.com>
2017-10-05 16:43:26 -0500

    Fixing utils in package

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ Install file for SciTokens project.
 import setuptools
 
 setuptools.setup(name="scitokens",
-                 version="0.2.1",
+                 version="0.2.2",
                  description="SciToken reference implementation library",
                  author_email="team@scitokens.org",
                  author="Brian Bockelman",
                  url="https://scitokens.org",
                  package_dir={"": "src"},
-                 packages=["scitokens"],
+                 packages=["scitokens", "scitokens.utils"],
                  scripts=['tools/scitokens-admin-create'],
                  install_requires=['cryptography',
                                    'PyJWT'],
