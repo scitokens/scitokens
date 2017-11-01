63c365152592d8b22d9bfc45c1d386d49fd75706
Derek Weitzel <djw8605@gmail.com>
2017-11-01 15:59:55 -0500

    Adding .utils module to packaging

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ Install file for SciTokens project.
 import setuptools
 
 setuptools.setup(name="scitokens",
-                 version="0.3.0",
+                 version="0.3.1",
                  description="SciToken reference implementation library",
                  author_email="team@scitokens.org",
                  author="Brian Bockelman",
                  url="https://scitokens.org",
                  package_dir={"": "src"},
-                 packages=["scitokens"],
+                 packages=["scitokens", "scitokens.utils"],
                  scripts=['tools/scitokens-admin-create-token',
                           'tools/scitokens-admin-create-key'],
                  install_requires=['cryptography',
