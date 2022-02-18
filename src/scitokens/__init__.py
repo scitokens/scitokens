f70cedee1d29d5dce98da496354b3c543a2aa6ce
Derek Weitzel <djw8605@gmail.com>
2022-02-18 12:56:16 -0600

    Bump version to 1.7.0

--- src/scitokens/__init__.py
+++ src/scitokens/__init__.py
@@ -6,4 +6,4 @@ Module for creating and using SciTokens.
 from .scitokens import SciToken, Validator, Enforcer, MissingClaims
 from .utils.config import set_config
 
-__version__ = "1.6.2"
+__version__ = "1.7.0"
