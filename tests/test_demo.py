a17542647774c5f50162649cb3026100bdccc01d
uyenle-gh <le_u1@denison.edu>
2023-03-06 22:19:52 -0500

    Include tests for parsde_token

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,28 +1,58 @@
+"""
+Test demo module 
+"""
+
 import scitokens.utils.demo
 import unittest
 
 
-class TestDemo(unittest.TestCase):
-    def test_valid_token(self):
+class TestToken(unittest.TestCase):
+    def test_valid_payload(self):
         """
         Test that the token matches the specified payload
         """
         payload = {
-            "key1": "value1",
-            "key2": "value2",
-            "key3": "value3",
+            "key1": "val1", 
+            "key2": "val2"
             }
-        
+
         token_serialized = scitokens.utils.demo.token(payload)
         token = scitokens.SciToken.deserialize(token_serialized)            # automatically call verify 
-    
+        
         # assert that the payload is part of the claims
         for key, value in payload.items(): 
             self.assertIn((key, value), token.claims())
 
-        # validate the token 
-        val = scitokens.Validator()
-        self.assertTrue(val.validate(token))
+
+    def test_empty_payload(self): 
+        """
+        Test token with empty payload
+        """    
+        payload = {}
+        token_serialized = scitokens.utils.demo.token(payload)  
+        token = scitokens.SciToken.deserialize(token_serialized) 
+
+
+class TestParsedToken(unittest.TestCase): 
+    def test_valid_parsed(self): 
+        """
+        Test that the parsed token matches the payload
+        """
+        payload = {
+            "key1": "val1", 
+            "key2": "val2"
+            }
+
+        token = scitokens.utils.demo.parsed_token(payload)
+        for key, value in payload.items(): 
+            self.assertIn((key, value), token.claims())
+
+    def test_empty_parsed(self): 
+        """
+        Test token with empty payload
+        """
+        payload = {}
+        token = scitokens.utils.demo.parsed_token(payload)
 
 
 if __name__ == '__main__':
